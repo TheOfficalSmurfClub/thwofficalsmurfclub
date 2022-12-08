ae3d6df3debb62f6da1c93183652f9d6de36a697
TheOfficalSmurfClub <119353984+TheOfficalSmurfClub@users.noreply.github.com>
2022-12-07 20:05:31 -0500

    Add files via upload

---
+++ me and liam/login.py
@@ -0,0 +1,8 @@
+print('hello fellow smurf. Please login in.')
+
+username = input('username')
+password = input('password')
+
+print('hello smurf ',username)
+
+close = input('to close correctly please press enter, thank you.')
---
+++ me and liam/signup.py
@@ -0,0 +1,19 @@
+print('Hi, this is the login page')
+
+name = input("what is your name?")
+age = input('how old are you?')
+Name = ('your name is ')
+messagePartName = (' and you were born on ')
+dateOfBirth = input('when is you birthday?')
+password = input('Create your password.')
+Message_part= ('. And your password is ')
+email_addres = input('Please enter YOUR email adress.')
+message_part = ('. and in case of an emergency we will contact you at ')
+print(Name + name + messagePartName + dateOfBirth + Message_part + password + message_part + email_addres)
+
+username = input('what whould you like to have your username?')
+occupation = input('what is your job in the Smurf Club')
+
+print("Smurf ",username,"nice to see your hard working as a",occupation)
+print('Hope you like this because your going to have to use it alot')
+end = input('this will close when you press enter.')
